717ebea334753ce30d0f38fe60dcda576cb76c58
Lozmep <lozano.mauriciop@gmail.com>
2022-11-20 11:32:44 -0500

    add test routes

--- index.js
+++ index.js
@@ -7,6 +7,17 @@ app.get('/', (req,res) => {
   res.send('Server works')
 })
 
+app.get('/nueva-ruta', (req,res) => {
+  res.send('Another endpoint')
+})
+
+app.get('/products', (req,res) => {
+  res.json({
+    name: 'Producto 1',
+    price: '1000'
+  })
+})
+
 app.listen(PORT, () => {
   console.log(`Server Listen at port ${PORT}`)
 })
