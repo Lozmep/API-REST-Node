88a976fc59457ccb8b8c961ab145470c9a943ddc
Lozmep <lozano.mauriciop@gmail.com>
2022-11-20 11:29:04 -0500

    create server with express

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('My APP')
+const express = require('express')
+
+const app = express()
+const PORT = 3000
+
+app.get('/', (req,res) => {
+  res.send('Server works')
+})
+
+app.listen(PORT, () => {
+  console.log(`Server Listen at port ${PORT}`)
+})
